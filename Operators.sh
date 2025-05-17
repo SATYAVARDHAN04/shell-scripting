726d8ce797f831b3f7e5c0d010f122ef51251b17
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-18 00:28:15 +0530

    operators

---
+++ Operators.sh
@@ -0,0 +1,6 @@
+num1=100
+num2=200
+
+sum=$((num1+num2))
+
+echo "Sum of the two numbers is $sum"
--- first_script.sh
+++ first_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 echo "do you know who am i $(whoami)"
 echo "hello world and INDIA!"
