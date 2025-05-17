793b4ee0ffe3647929b14c02742e418840c531b2
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-18 00:43:31 +0530

    operators

---
+++ Operators2.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Enter the number1 = "
+read num1
+
+echo "Enter the number2 = "
+read num2
+
+echo "Enter the number3 = "
+read num3
+
+SUM=$((num1+num2+num3))
+
+echo "THE SUM OF ALL NUMBERS = $SUM"
