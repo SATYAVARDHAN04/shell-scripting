b0ccf25b55cf0ddcf1786d19e595767a3d2b8324
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-18 17:00:47 +0530

    mysql-installation

--- condition2.sh
+++ condition2.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 USERNAME=$(id -u)
 
-if[ "$USERNAME" -eq 0 ]; then
-    echo "Sorry cannot perform"
-
+if [ "$USERNAME" -eq 0 ]; then
+    echo "Sorry, cannot perform"
 else
     echo "Can perform"
+fi
