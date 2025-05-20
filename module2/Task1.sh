dd1937106b41ff542d3a72b2c12d21c600112f50
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-20 17:22:09 +0530

    module2

---
+++ module2/Task1.sh
@@ -0,0 +1,9 @@
+# 1. Perform all arthimatic operations
+
+read -p "Enter values for 2 variables" a b
+echo "Addition = $((a + b))"
+echo "Subtraction = $((a - b))"
+echo "Multiplication = $((a * b))"
+echo "Division = $((a / b))"
+echo "Modulus = $((a % b))"
+echo "Exponentiation = $((a ** b))"
