f30177ee55bef532d615aeeda266938882343983
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-22 18:49:54 +0530

    mysql-installation

--- roboshop/Mysql.sh
+++ roboshop/Mysql.sh
@@ -1,18 +1,43 @@
-#! /bin/bash
+#!/bin/bash
 
-USER=$(id -u)
-if [ $USER -ne 0 ]; then
-    echo "Error: Please run the script with sudo or root user"
-    exit 1
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python" "nginx" "httpd")
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-    echo "Installing MySQL"
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-apt install mysql-server -y
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to install MySQL"
-    exit 1
-else
-    echo "MySQL installed successfully"
-fi
+#for package in ${PACKAGES[@]}
+for package in $@; do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed... going to install it" | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
+    fi
+done
