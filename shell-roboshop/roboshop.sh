b06c988539505e0c268538db0e8d44e002a4c48d
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-24 23:44:39 +0530

    roboshop

--- shell-roboshop/roboshop.sh
+++ shell-roboshop/roboshop.sh
@@ -3,55 +3,56 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-03d93947d06697ec0"
 SUBNET_ID="subnet-01be324df23d176f9"
-INSTANCES=("MONGODB" "REDIS" "MYSQL" "RABBITMQ" "USER" "CART" "DISPATCH" "CATALOGUE" "SHIPPING" "PAYMENT" "FRONTEND")
-
 ZONE_ID="Z05550482K6DBOPR7GPPB"
 DOMAIN_NAME="satyology.site"
 
-for instance in "${INSTANCES[@]}"; do
-        echo "Launching instance: $instance"
+INSTANCES=("MONGODB" "REDDIS" "MYSQL" "RABBITMQ" "CATALOGUE" "USER" "CART" "SHIPPING" "PAYMENT" "DISPATCH" "FRONTEND")
+
+for INSTANCE in "${INSTANCES[@]}"; do
+        echo "Launching instance $INSTANCE"
+
         INSTANCE_ID=$(aws ec2 run-instances \
-                --image-id $AMI_ID \
+                --image-id "$AMI_ID" \
                 --instance-type t2.micro \
-                --subnet-id $SUBNET_ID \
-                --security-group-ids $SG_ID \
-                --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+                --subnet-id "$SUBNET_ID" \
+                --security-group-ids "$SG_ID" \
+                --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
                 --query "Instances[0].InstanceId" \
                 --output text)
 
         echo "Instance ID: $INSTANCE_ID"
 
-        echo "Waiting for instance to initialize..."
-        sleep 10
+        # Wait for the instance to be in running state
+        aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
 
-        if [ "$instance" != "FRONTEND" ]; then
-                IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-                        --query "Reservations[0].Instances[0].PrivateIpAddress" \
-                        --output text)
-                RECORD_NAME="$instance.$DOMAIN_NAME"
-        else
-                IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+        if [ "$INSTANCE" == "FRONTEND" ]; then
+                IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
                         --query "Reservations[0].Instances[0].PublicIpAddress" \
                         --output text)
                 RECORD_NAME="$DOMAIN_NAME"
+        else
+                IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
+                        --query "Reservations[0].Instances[0].PrivateIpAddress" \
+                        --output text)
+                RECORD_NAME="$INSTANCE.$DOMAIN_NAME"
         fi
 
-        echo "$instance IP address: $IP"
+        echo "Updating Route53 Record: $RECORD_NAME -> $IP"
 
         aws route53 change-resource-record-sets \
-                --hosted-zone-id $ZONE_ID \
-                --change-batch '{
-            "Comment": "Creating or Updating a record set"
-            ,"Changes": [{
-                "Action": "UPSERT",
-                "ResourceRecordSet": {
-                    "Name": "'"$RECORD_NAME"'",
-                    "Type": "A",
-                    "TTL": 1,
-                    "ResourceRecords": [{
-                        "Value": "'"$IP"'"
+                --hosted-zone-id "$ZONE_ID" \
+                --change-batch "{
+            \"Comment\": \"Creating or Updating a record set\",
+            \"Changes\": [{
+                \"Action\": \"UPSERT\",
+                \"ResourceRecordSet\": {
+                    \"Name\": \"$RECORD_NAME\",
+                    \"Type\": \"A\",
+                    \"TTL\": 60,
+                    \"ResourceRecords\": [{
+                        \"Value\": \"$IP\"
                     }]
                 }
             }]
-        }'
+        }"
 done
