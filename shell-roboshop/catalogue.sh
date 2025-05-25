7091839a5b497d4a339b98c7ff334a46d137aa30
Manikonda Satya Vardhan <svardhan278@gmail.com>
2025-05-25 16:32:15 +0530

    roboshop

---
+++ shell-roboshop/catalogue.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p "$LOGS_FOLDER"
+echo "Script started executing at: $(date)" | tee -a "$LOG_FILE"
+
+# Root check
+if [ "$USERID" -ne 0 ]; then
+    echo -e "$R ERROR: Run the script as root $N" | tee -a "$LOG_FILE"
+    exit 1
+else
+    echo -e "$G Running with root access $N" | tee -a "$LOG_FILE"
+fi
+
+# Validation function
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 ... $G SUCCESS $N" | tee -a "$LOG_FILE"
+    else
+        echo -e "$2 ... $R FAILURE $N" | tee -a "$LOG_FILE"
+        exit 1
+    fi
+}
+
+# NodeJS setup
+dnf module disable nodejs -y &>>"$LOG_FILE"
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>"$LOG_FILE"
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>>"$LOG_FILE"
+VALIDATE $? "Installing nodejs"
+
+# roboshop user setup
+id roboshop &>>"$LOG_FILE"
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>"$LOG_FILE"
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "User 'roboshop' already exists ... $Y SKIPPING $N" | tee -a "$LOG_FILE"
+fi
+
+# App setup
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>"$LOG_FILE"
+VALIDATE $? "Downloading catalogue"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/catalogue.zip &>>"$LOG_FILE"
+VALIDATE $? "Unzipping catalogue"
+
+npm install &>>"$LOG_FILE"
+VALIDATE $? "Installing Node.js dependencies"
+
+cp "$SCRIPT_DIR/catalogue.service" /etc/systemd/system/catalogue.service &>>"$LOG_FILE"
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>"$LOG_FILE"
+systemctl enable catalogue &>>"$LOG_FILE"
+systemctl start catalogue &>>"$LOG_FILE"
+VALIDATE $? "Starting catalogue service"
+
+# MongoDB client and data setup
+cp "$SCRIPT_DIR/mongo.repo" /etc/yum.repos.d/mongo.repo &>>"$LOG_FILE"
+dnf install mongodb-mongosh -y &>>"$LOG_FILE"
+VALIDATE $? "Installing MongoDB client"
+
+STATUS=$(mongosh --host mongodb.satyology.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")' | grep -Eo '^[0-9]+$')
+if [ "$STATUS" -lt 0 ]; then
+    mongosh --host mongodb.satyology.site </app/db/master-data.js &>>"$LOG_FILE"
+    VALIDATE $? "Importing MongoDB data"
+else
+    echo -e "MongoDB data already exists ... $Y SKIPPING $N" | tee -a "$LOG_FILE"
+fi
+
+echo "Script completed at: $(date)" | tee -a "$LOG_FILE"
--- shell-roboshop/mongodb.sh
+++ shell-roboshop/mongodb.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+N="\033[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" .sh)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+mkdir -p "$LOGS_FOLDER"
+echo "Script started executing at: $(date)" | tee -a "$LOG_FILE"
 
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]; then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+# Check root privileges
+if [ "$USERID" -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a "$LOG_FILE"
+    exit 1
 else
-    echo "You are running with root access" | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a "$LOG_FILE"
 fi
 
-# validate functions takes input as exit status, what command they tried to install
+# Validation function
 VALIDATE() {
     if [ $1 -eq 0 ]; then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a "$LOG_FILE"
     else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILURE $N" | tee -a "$LOG_FILE"
         exit 1
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongodb.repo
+cp mongo.repo /etc/yum.repos.d/mongodb.repo &>>"$LOG_FILE"
 VALIDATE $? "Copying MongoDB repo"
 
-dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing mongodb server"
+dnf install mongodb-org -y &>>"$LOG_FILE"
+VALIDATE $? "Installing MongoDB server"
 
-systemctl enable mongod &>>$LOG_FILE
+systemctl enable mongod &>>"$LOG_FILE"
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod &>>$LOG_FILE
+systemctl start mongod &>>"$LOG_FILE"
 VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>"$LOG_FILE"
 VALIDATE $? "Editing MongoDB conf file for remote connections"
 
-systemctl restart mongod &>>$LOG_FILE
+systemctl restart mongod &>>"$LOG_FILE"
 VALIDATE $? "Restarting MongoDB"
+
+echo "Script completed at: $(date)" | tee -a "$LOG_FILE"
